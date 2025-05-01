8a68e0e2211f7ab8bf86b724dcf0690f101a6bac
Chingun Amarbaatar <chingunee.dev@gmail.com>
2025-05-01 15:36:39 +0800

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-abi-exporter");
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    ethereum_sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
+  abiExporter: {
+    path: "./app/abi",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    spacing: 2,
+  },
+};
---
+++ ignition/modules/HelixStakedICP.js
@@ -0,0 +1,9 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const defaultAdmin = "0x3989BCC4a9A4E356265AcC658fB10Dfb3a86ddd7";
+const minter = "0xf9bb58B6725e7e62E5786b6f670FBfB1a52d48aD";
+
+module.exports = buildModule("HelixStakedICPModule", (m) => {
+  const hstICP = m.contract("HelixStakedICP", [defaultAdmin, minter]);
+  return { hstICP };
+});
